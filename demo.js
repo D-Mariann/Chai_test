0260aa71bcee6ec466df2f0ed5854df818ce73e4
D-Mariann <119792440+D-Mariann@users.noreply.github.com>
2024-11-27 14:50:10 +0300

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,95 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+
+// создаем объект приложения
+const app = express()
+// parse application/json
+app.use(bodyParser.json())
+
+const port = 3000
+
+let users = [
+  {
+    id: 1,
+    name: 'John',
+    balance: 100.0,
+  },
+]
+exports.users = users;
+
+// список юзеров
+app.get('/users', (req, res) => {
+  res.send({ status: 'ok', users:users })
+})
+
+// информация об одном юзере
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id
+  const idx = id - 1
+  if (idx in users) {
+    res.send({ user: users[idx] })
+  } else {
+    res.status(404)
+    res.send({ status: 'not found' })
+  }
+})
+
+// регистрация нового юзера
+app.post('/users', (req, res) => {
+  const user = {
+    id: users.length + 1,
+    name: req.body.name,
+    balance: 0.0,
+  }
+  users.push(user)
+  res.send({ status: 'ok', user: user })
+})
+
+// юзер совершает покупку стоимостью price
+app.post('/users/:id/buy/:price', (req, res) => {
+  const id = parseInt(req.params.id)
+  const idx = id - 1
+  if (!(idx in users)) {
+    res.send({ status: 'error', text: 'user not found' })
+    return
+  }
+  const user = users[idx]
+  const price = parseFloat(req.params.price)
+  if (user.balance < price) {
+    res.send({ status: 'error', text: 'not enough money' })
+    return
+  }
+  user.balance -= price
+  res.send({
+    status: 'ok',
+    user: user,
+  })
+})
+
+// дать юзеру денег безвозмездно
+app.post('/users/:id/faucet/:amount', (req, res) => {
+  const id = parseInt(req.params.id)
+  const idx = id - 1
+  if (!(idx in users)) {
+    res.send({ status: 'error', text: 'user not found' })
+    return
+  }
+  const user = users[idx]
+  const amount = parseFloat(req.params.amount)
+
+  user.balance += amount
+  res.send({
+    status: 'ok',
+    user: user,
+  })
+})
+
+// пинг системы
+app.get('/ping', (req, res) => {
+  res.send({status: 'ok', text: 'pong'})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
---
+++ new1.test.js
@@ -0,0 +1,113 @@
+const expect = require('chai').expect
+const app = require('./service/demo_service')
+let users = require('../demo')
+
+
+describe('demo', () => {
+    describe('users/:id', () => {
+        it("Has a user info by id", async () => {      
+            let r = await app.get("users/1");
+            expect(r).to.have.property('status', 200);
+
+            const res = await r.json();         
+            expect(res.user.id).equal(1);
+            expect(res.user.name).equal('John');
+            expect(res.user.balance).equal(100.0);
+        });
+        it('Return a 404 if invalid user id', async () => {
+            let r = await app.get("users/2");
+            expect(r).to.have.property('statusText', 'Not Found');
+            expect(r).to.have.property('status', 404);
+            const res = await r.json();            
+            expect(res.status).equal('not found');            
+        });
+    });
+
+    describe('users', function() {
+        it('Registering a new user', async function() {
+            const res = await app.post('users', { name: 'Alice' });            
+            const data = await res.json();
+            expect(data.status).to.equal('ok');
+            expect(data.user).to.deep.equal({ id: 2, name: 'Alice', balance: 0.0 });
+        });              
+           
+        it('Registering a new user2', async function() {
+            const res = await app.post('users', { name: 'Gena' });            
+            expect(res.status).equal(200);
+            const data = await res.json();
+            expect(data.status).equal('ok');
+            expect(data.user.name).equal('Gena');
+            expect(data.user.id).equal(3);
+            expect(data.user.balance).equal(0);
+        });   
+    });
+
+    describe('/users/:id/buy/:price', function() {
+        let len = users.lenght
+        let bal = users
+        it('User makes a purchase worth the price', async function() {
+            let id = 1
+            let price = 10;
+            let sale = {id: id, price: price}
+            const res = await app.post(`users/${id}/buy/${price}`, sale);
+            let idx = users.users[id-1].id
+            let nameU = users.users[id-1].name
+            let bal = users.users[id-1].balance
+            expect(res.status).to.equal(200);
+            const data = await res.json();            
+            expect(data.user).to.deep.equal({ id: idx, name: nameU, balance: bal });
+        });   
+
+        it('User not found', async function() {
+            let id = 9
+            let price = 10;
+            let sale = {id: id, price: price}
+            const res = await app.post(`users/${id}/buy/${price}`, sale);
+            expect(res.status).to.equal(200);
+            const data = await res.json();                        
+            expect(data).to.deep.equal({ status: 'error', text: 'user not found' });
+        });   
+        
+        it('User has not enough money', async function() {
+            let id = 2
+            let price = 10;
+            let sale = {id: id, price: price}
+            const res = await app.post(`users/${id}/buy/${price}`, sale);
+            expect(res.status).to.equal(200);
+            const data = await res.json();                
+            expect(data).to.deep.equal({ status: 'error', text: 'not enough money' });
+        });   
+    });
+
+
+    describe('/users/:id/faucet/:amount', function() {
+                
+        it('Give the user money for free', async function() {
+            let id = 1
+            let amount = 10;
+            let gift = {id: id, amount: amount}
+            const res = await app.post(`users/${id}/faucet/${amount}`, gift);
+
+            let idx = users.users[id-1].id
+            let nameU = users.users[id-1].name
+            let bal = users.users[id-1].balance
+            console.log('bal : ', bal)
+            expect(res.status).to.equal(200);
+            const data = await res.json();
+            expect(data.status).equal('ok');
+            expect(data.user).to.deep.equal({ id: idx, name: nameU, balance: bal });
+        }); 
+
+        it('User not found', async function() {
+            let id = 7
+            let amount = 10;
+            let gift = {id: id, amount: amount};
+            const res = await app.post(`users/${id}/faucet/${amount}`, gift);
+            expect(res.status).to.equal(200);        
+            const data = await res.json();                      
+            expect(data).to.deep.equal({ status: 'error', text: 'user not found' });
+        });     
+    });
+
+});
+
